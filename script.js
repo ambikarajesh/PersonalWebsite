de0921bc04d7b7fc1e6e702810e7e5c5cecade66
ambikarajesh <ambikula@gmail.com>
2018-06-25 21:31:26 -0700

    commit #1

---
+++ script.js
@@ -0,0 +1,15 @@
+function homePage(){
+    alert("clicked");
+}
+
+function aboutMePage(){
+    alert("clicked");
+}
+
+function portfolioPage(){
+    alert("clicked");
+}
+
+function contactUsPage(){
+    alert("clicked");
+}
